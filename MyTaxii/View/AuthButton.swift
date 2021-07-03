9d79f3d71f120c17f63b812b8324c7a6b321efc8
roxana-solhjoo <Roxanslj44@gmail.com>
2021-07-03 14:41:10 +0200

    Final Commit

---
+++ MyTaxii/View/AuthButton.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class AuthButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 5
+        backgroundColor = .mainBlueTint
+        setTitleColor(.white, for: .normal)
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
